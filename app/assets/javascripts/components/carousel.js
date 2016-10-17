8575ad016142d591435e89fd9bb80604a528332c
LMafra <chagas.lucas.mafra@gmail.com>
2016-10-17 10:50:20 -0200

    Finalizando Nossos clientes/parceiros

--- app/assets/javascripts/components/carousel.js
+++ app/assets/javascripts/components/carousel.js
@@ -1,19 +1,5 @@
 var carousel;
 $(document).ready(function () {
-    carousel = $("ul");
+    carousel = $(".ul-principios");
     carousel.itemslide();
-});
-
-$(document).ready(function() {
- 
-  $("#owl-demo").owlCarousel({
- 
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
- 
-      items : 4,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
- 
-  });
- 
 });
