61c0e518e068b6f8272f57dfe8ec123fe84c95f4
LMafra <chagas.lucas.mafra@gmail.com>
2016-11-01 00:17:29 -0200

    Falta consertar o footer e o banner

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,17 +1,10 @@
 $(function() {
 	$('nav.menu a').bind('click',function(event){
-		console.log("Olá");
 		var $anchor = $(this);
 
 		$('html, body').stop().animate({
 			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1500);
-		/*
-		if you don't want to use the easing effects:
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
 		}, 1000);
-		*/
 		event.preventDefault();
 	});
 });
